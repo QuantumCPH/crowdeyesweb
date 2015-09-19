93b7bc017dba301b36cdc4a6d53118251cfcc287
Muhammad Riaz <riaz_qadeer90@yahoo.com>
2015-09-19 13:47:26 +0500

    fdgdfg

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'crowdeyes');
+define('DB_NAME', 'crowdeyes-parallax');
 
 /** MySQL database username */
 define('DB_USER', 'root');
