b48e3490ee3af46ab8e8714168af579090afaaf1
Muhammad Riaz <riaz_qadeer90@yahoo.com>
2015-09-19 13:25:59 +0500

    fdgdfg

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'crowdeyes');
+define('DB_NAME', 'crowdeyes-parallax');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'SsH@_!1991!');
+define('DB_PASSWORD', 'UtlK&@#as@!&12');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
