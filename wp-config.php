402a5bc4a2f88933fb5737570f283ea19fee9ee3
Muhammad Riaz <riaz_qadeer90@yahoo.com>
2015-10-20 12:38:17 +0500

    aaaa

--- wp-config.php
+++ wp-config.php
@@ -19,22 +19,22 @@
 
 
 
-define('DB_NAME', 'crowd');
-
-/** MySQL database username */
-define('DB_USER', 'root');
-
-/** MySQL database password */
-define('DB_PASSWORD', '');
-
-
-//define('DB_NAME', 'crowdeyes');
+//define('DB_NAME', 'crowd');
 //
 ///** MySQL database username */
 //define('DB_USER', 'root');
 //
 ///** MySQL database password */
-//define('DB_PASSWORD', 'UtlK&@#as@!&12');
+//define('DB_PASSWORD', '');
+
+
+define('DB_NAME', 'crowdeyes');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'UtlK&@#as@!&12');
 
 
 /** MySQL hostname */
