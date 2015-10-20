171edc2cc550c0048f27b90ecce826173011e78a
Muhammad Riaz <riaz_qadeer90@yahoo.com>
2015-10-20 17:28:47 +0500

    aaa

--- wp-content/themes/crowdeyes/page-about.php
+++ wp-content/themes/crowdeyes/page-about.php
@@ -83,7 +83,7 @@ p{ margin-top:10px;}
 	
         <div class="section" id="about-us">
             <h2><?php the_title(); ?></h2>
-            <p><?php echo $post->post_content; ?></p>
+            <p><?php echo $post->post_content; the_content(); ?></p>
         </div>
         
 	
