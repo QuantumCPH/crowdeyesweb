f43c7045474df59809acdcf1a223ebeef8d7eebf
Muhammad Riaz <riaz_qadeer90@yahoo.com>
2015-12-10 20:15:46 +0500

    aaa

--- wp-content/themes/crowdeyes/page-danish.php
+++ wp-content/themes/crowdeyes/page-danish.php
@@ -356,7 +356,7 @@ app.controller('customersCtrl', function($scope, $http) {
     </div>
     <div class="container">
       <div class="regPlan">
-        <?php echo do_shortcode('[contact-form-7 id="83" title="Plan Register"]'); ?>
+        <?php echo do_shortcode('[contact-form-7 id="106" title="Plan Register Danish"]'); ?>
       </div>
     </div>
 
