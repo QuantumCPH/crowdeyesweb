a4a9f3cb81d42d038cc1d746c4c829e837bbdd9c
Muhammad Riaz <riaz_qadeer90@yahoo.com>
2015-12-15 14:49:38 +0500

    aaaa

--- wp-content/themes/crowdeyes/page-home.php
+++ wp-content/themes/crowdeyes/page-home.php
@@ -12,10 +12,9 @@
  * @since Twenty Thirteen 1.0
  */
 
-?>
+get_header(); ?>
 <html>
 <head>
-  <?php get_header(); ?>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
   <meta name="description" content="" />
@@ -39,7 +38,7 @@
   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
   <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
   <![endif]-->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script type='text/javascript' src='https://code.jquery.com/jquery-1.11.1.js'></script>
   <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
   <link href='https://fonts.googleapis.com/css?family=Alegreya+Sans:400,300,900,700,500,800' rel='stylesheet' type='text/css'>
 
