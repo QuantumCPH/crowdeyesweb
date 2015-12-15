73649f30d9d8eae188d94e1633d9ef4b0d1c4a9c
Muhammad Riaz <riaz_qadeer90@yahoo.com>
2015-12-15 14:48:35 +0500

    aaaa

--- wp-content/themes/crowdeyes/page-home.php
+++ wp-content/themes/crowdeyes/page-home.php
@@ -39,7 +39,7 @@
   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
   <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
   <![endif]-->
-  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
   <link href='https://fonts.googleapis.com/css?family=Alegreya+Sans:400,300,900,700,500,800' rel='stylesheet' type='text/css'>
 
