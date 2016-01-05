3c60d9f4cdad07fab9bcaed79ea660e015fe19b1
Muhammad Riaz <riaz_qadeer90@yahoo.com>
2016-01-05 14:53:12 +0500

    aaaaa

--- location.php
+++ location.php
@@ -1,30 +1,12 @@
 <?php
 
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-   echo $ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-   echo $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
+
    echo $ip = $_SERVER['REMOTE_ADDR'];
-}
 
 $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}"));
 
 
 echo "{$details->city}";
 
- $address = "Lahore";
-$url = "http://maps.google.com/maps/api/geocode/json?address=$address&sensor=false&region=India";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-$response = curl_exec($ch);
-curl_close($ch);
-$response_a = json_decode($response);
-echo @$lat = $response_a->results[0]->geometry->location->lat;
-echo "<br />";
-echo @$long = $response_a->results[0]->geometry->location->lng;
+ 
 ?>
