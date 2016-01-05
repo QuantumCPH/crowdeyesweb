3866ae4f92750a4fd3b8dc4ce0f6722f96c17c87
Muhammad Riaz <riaz_qadeer90@yahoo.com>
2016-01-05 15:48:21 +0500

    aaaaa

--- wp-content/themes/crowdeyes/page-home.php
+++ wp-content/themes/crowdeyes/page-home.php
@@ -141,10 +141,10 @@ get_header(); ?>
       <?php
       $url = json_decode(file_get_contents("http://api.ipinfodb.com/v3/ip-city/?key=4412550d3b0efa3aa92c6560acd857b6dbd45d82456feee448559e743094cadd&ip=".$_SERVER['REMOTE_ADDR']."&format=json"));
 
-      echo "<tr><td>LATITUTE:</td><td>";
-      echo $url->latitude;
-      echo "</td></tr><tr><td>LONGITUDE:</td><td>";
-      echo $url->longitude;
+
+     // echo $url->latitude;
+
+      //echo $url->longitude;
 
 ?>
 
