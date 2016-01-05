e9906e48c871095285caf92a56ffa7e693701845
Muhammad Riaz <riaz_qadeer90@yahoo.com>
2016-01-05 15:02:05 +0500

    aaaaa

--- location.php
+++ location.php
@@ -20,3 +20,17 @@ if($query && $query['status'] == 'success') {
   echo 'Unable to get location';
 }
 ?>
+
+<?php
+# Collect a specific users GEOIP info
+$info = geoip_record_by_name($_SERVER['REMOTE_ADDR']);
+print_r ($info);
+
+# To get the info from one specific field
+$country = $info['country_name'];
+echo $country;
+
+# To combine information from the array into a string
+$info = implode("/", $info);
+echo $info;
+?>
