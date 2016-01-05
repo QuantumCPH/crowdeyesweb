ded011f11a25625262bc1629da5f86bac5f41757
Muhammad Riaz <riaz_qadeer90@yahoo.com>
2016-01-05 14:58:28 +0500

    aaaaa

--- location.php
+++ location.php
@@ -1,12 +1,6 @@
 <?php
 
 
-   echo $ip = $_SERVER['REMOTE_ADDR'];
-
-$details = json_decode(file_get_contents("http://ipinfo.io/{$ip}"));
-
-
-echo "{$details->city}";
 
 
 ?>
@@ -15,7 +9,7 @@ echo "{$details->city}";
 $ip = $_REQUEST['REMOTE_ADDR']; // the IP address to query
 $query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip));
 if($query && $query['status'] == 'success') {
-  echo 'Hello visitor from '.$query['country'].', '.$query['city'].'!';
+  echo $query['city'];
 } else {
   echo 'Unable to get location';
 }
