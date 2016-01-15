7fecb67bc7a98e92623cab1d4ef26a56605fee5c
Muhammad Riaz <riaz_qadeer90@yahoo.com>
2016-01-15 16:16:00 +0500

    aaaa

--- wp-content/themes/crowdeyes/page-home.php
+++ wp-content/themes/crowdeyes/page-home.php
@@ -285,7 +285,7 @@ get_header(); ?>
 
 
       <style>
-        .imgFirst{ height:30px; width:30px; background: #fff; text-align: center;}
+        .imgFirst{ height:30px; width:30px; background: #ccc; text-align: center;}
         .imgFirst img{max-width: 100%; max-height: 100%; border-radius: 50%}
         .lostImg2{ height:90px; width:95px; border-radius: 5px; border: solid 0px #fff;}
       </style>
