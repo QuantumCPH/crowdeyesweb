ce7e46b451b7164d8465d653c31550940ebdf2b5
Muhammad Riaz <riaz_qadeer90@yahoo.com>
2016-01-21 14:58:38 +0500

    aaaa

--- Q_chat/upload_file.php
+++ Q_chat/upload_file.php
@@ -8,7 +8,7 @@ if(move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder))
  
  {
  
- echo "http://ec2-52-29-132-13.eu-central-1.compute.amazonaws.com/Q_chat/Q_media/". basename( $_FILES['file']['name'])."";
+ echo "http://108.163.184.34/crowdeyes-parallax/Q_chat/Q_media/". basename( $_FILES['file']['name'])."";
  
  }
  
