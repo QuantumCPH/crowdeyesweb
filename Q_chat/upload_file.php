cd82d4531456b020a7b6f25f9b7aa122da4c5d47
Muhammad Riaz <riaz_qadeer90@yahoo.com>
2016-01-21 15:36:32 +0500

    aaaa

--- Q_chat/upload_file.php
+++ Q_chat/upload_file.php
@@ -2,15 +2,13 @@
  
  $targetfolder = "Q_media/";
  
- //$targetfolder = $targetfolder . basename( $_FILES['file']['name']) ;
- $targetfolder = $targetfolder . basename($_FILES["fileToUpload"]["name"]);
+ $targetfolder = $targetfolder . basename( $_FILES['file']['name']) ;
 
-if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetfolder))
-//if(move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder))
+if(move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder))
  
  {
  
- echo "<img src=' http://108.163.184.34/crowdeyes-parallax/Q_chat/Q_media/". basename( $_FILES['fileToUpload']['name'])." '>";
+ echo "http://108.163.184.34/crowdeyes-parallax/Q_chat/Q_media/". basename( $_FILES['file']['name'])."";
  
  }
  
@@ -21,10 +19,3 @@ if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetfolder))
  }
  
  ?>
- 
- <form action="" method="post" enctype="multipart/form-data">
- 
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
-	
-</form>
